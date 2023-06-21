5eddad3ff019711ea27e5f0e6e1ad6851bfbbe31
Yehoshua Tarshish <a0526741852@gmail.com>
2023-06-21 12:44:32 +0300

    updated responses statuses &  login & register endpoints

--- contollers/auth.controller.js
+++ contollers/auth.controller.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcrypt");
-const authService = require("./auth.controller");
+const authService = require("../middleware/auth");
 const userSchema = require("../models/user.model");
 
 class UserClass {
@@ -22,20 +22,22 @@ class UserClass {
   }
 
   static async login(req, res) {
-    //TODO: wrap everything with try catch
-    const user = await userSchema.findOne({ email: req.body.email });
-    //TODO: change to unauthorized
-    //TODO: You're sending 200 here, change the response to a valid http code
-    if (!user) res.send("wrong email or password");
+    try {
+      const user = await userSchema.findOne({ email: req.body.email });
+      if (!user) {
+        res.status(401).send("Unauthorized");
+        return;
+      }
 
-    const validation = await bcrypt.compare(req.body.password, user.password);
-    if (validation)
-      res.send({
-        token: await authService.createToken(req.body.email),
-      });
-    //TODO: you're sending 200 here, its wrong
-    else res.send("wrong email or password");
+      const validation = await bcrypt.compare(req.body.password, user.password);
+      if (validation)
+        res.send({
+          token: await authService.createToken(req.body.email),
+        });
+      else res.status(401).send("Unauthorized");
+    } catch (err) {
+      res.sendStatus(500);
+    }
   }
 }
-
 module.exports = UserClass;
